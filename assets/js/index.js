eb37f2f535b3769ebe4a4322045d355f9ef5143d
EvgenStr <juve2910@gmail.com>
2021-04-04 00:26:41 +0300

    add shift func

--- assets/js/index.js
+++ assets/js/index.js
@@ -28,24 +28,32 @@ function MyArrayProto() {
   };
   this.isMyArray = function isMyArray() {
     return this instanceof MyArray;
-  }
+  };
   this.unshift = function unshift(...params) {
     this.forEach((elem, i, arr) => {
       arr[i + params.length] = elem;
-    })
-
+    });
     for (let i = 0; i < params.length; i++) {
       this[i] = params[i]
-    }
-
+    };
     return this.length += params.length;
-
+  };
+  this.shift = function shift() {
+    if (this.length === 0) return this;
+    const result = this[0];
+    delete this[0];
+    this.length--;
+    this.forEach((elem, i, arr) => {
+      arr[i] = arr[i+1];
+    });
+    delete this[this.length];
+    return result;
   }
 }
 
 const arr = new MyArray(1, 2, 3, 4, 5);
 
-arr.unshift(2, 3, 6);
+// arr.unshift(2, 3, 6);
 console.log(arr)
 
 
